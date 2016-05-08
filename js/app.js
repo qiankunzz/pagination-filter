5aea7b0862d0bac7b3143c83b86b478571dca902
qiankunzz <qiankunzz@gmail.com>
2016-05-08 09:15:13 -0400

    Finalizing the project
    Added Semicolon;
    Applied toLowerCase search;

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // Store Original html
 var students = $(".student-item");
 var studentNumber = students.length;
@@ -17,11 +18,12 @@ var searchInput = "";
 
 searchStudents = function($students) {
   // cycle through each element, and show it if there's is a match
-  $(".student-list").empty()
+  $(".student-list").empty();
+  var i;
   for ( i = 0; i < studentNumber; i++ ) {
-    var name, email, info
-    name =  students.eq(i).find("h3").html();
-    email = students.eq(i).find("span").html();
+    var name, email, info;
+    name =  students.eq(i).find("h3").html().toLowerCase();
+    email = students.eq(i).find("span").html().toLowerCase();
     info = name + " " + email;
     var list = students.eq(i)[0];
    if (info.includes(searchInput)) {
@@ -30,8 +32,8 @@ searchStudents = function($students) {
    }
   }
   if ($(".student-item").length === 0) {
-    console.log("No Result Found")
-    var message = "<p>No result found, please search again</p>"
+    console.log("No Result Found");
+    var message = "<p>No result found, please search again</p>";
     $(".student-list").append(message);
   }
   makeButtons();
@@ -46,6 +48,7 @@ function setActivePage(n) {
     $(".student-item").slice(x-10,x).fadeIn();
   });
   // take out the active class from the previous button
+  var i;
   for (i = 0; i < $("#page-number li").length; i++) {
     $("#page-number li a").eq(i).attr("class","");
   }
@@ -58,6 +61,7 @@ function setActivePageSearch() {
   $(".student-item").hide();
   $(".student-item").slice(0,10).show();
   // take out the active class from the previous button
+  var i;
   for (i = 0; i < $("#page-number li").length; i++) {
     $("#page-number li a").eq(i).attr("class","");
   }
@@ -74,27 +78,27 @@ function makeButtons() {
   var pageNumber = Math.floor(listNumber/10) + 1;
   for (i = 0; i < pageNumber; i++) {
     var n = i+1;
-  htmlButton += "<li><a class=''>" + n + "</a></li>"
-};
+  htmlButton += "<li><a class=''>" + n + "</a></li>";
+}
 $("#page-number").html(htmlButton);
 }
 
 
 // Bind each button with a click event
 function bindButtons() {
-  var i
+  var i;
   for (i = 0; i < $("#page-number li").length; i++) {
   //  ???
  $("#page-number li a").eq(i).click(function() {
       var activePageNumber = $(this).html();
       setActivePage(activePageNumber);
     });
-  };
+  }
 }
 
 // Bind Type to create value
 $("#search-input").keyup(function() {
-  searchInput = $(this).val();
+  searchInput = $(this).val().toLowerCase();
   //Extra Credit #2: dynamically filtering
   searchStudents();
 });
@@ -108,7 +112,7 @@ $("#search-input").keyup(function(e) {
 // bind search button with search event
 $("#search-button").click(function() {
   searchStudents();
-})
+});
 
 
 makeButtons();
